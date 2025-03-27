0ed7059a1be74dc359469e4255fab78a9728db31
박상호 <psangho76@gmail.com>
2025-03-27 10:07:43 +0900

    :lipstick: Design: 팀 컬러 UI 적용
    #JIRA-S12P21B206-53

---
+++ frontend/react-native/app/src/styles/teamColors.ts
@@ -0,0 +1,56 @@
+// src/styles/teamColors.ts
+export const teamColors = {
+  KIA: {
+    primary: "#e30613",
+    secondary: "#000000",
+    background: "#ffffff",
+  },
+  DOOSAN: {
+    primary: "#131230",
+    secondary: "#CD0320",
+    background: "#ffffff",
+  },
+  LG: {
+    primary: "#C30452",
+    secondary: "#000000",
+    background: "#ffffff",
+  },
+  SAMSUNG: {
+    primary: "#1428A0",
+    secondary: "#ffffff",
+    background: "#E10600",
+  },
+  KT: {
+    primary: "#000000",
+    secondary: "#E10600",
+    background: "#FFB81C",
+  },
+  SSG: {
+    primary: "#E10600",
+    secondary: "#ffffff",
+    background: "#FFB81C",
+  },
+  LOTTE: {
+    primary: "#002B70",
+    secondary: "#E10600",
+    background: "#ffffff",
+  },
+  Hanwha: {
+    primary: "#FF6600",
+    secondary: "#003057",
+    background: "#ffffff",
+  },
+  NC: {
+    primary: "#1E4D92",
+    secondary: "#FFB81C",
+    background: "#73C8F0",
+  },
+  Kiwoom: {
+    primary: "#6A0E21",
+    secondary: "#ffffff",
+    background: "#FFCCCB",
+  },
+};
+
+// 기본값 설정
+export const defaultTeam = "NC";
