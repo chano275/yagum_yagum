c3a3cf4c7e471c0963dbbc8c777df93f5346eb95
구민석 <kb908@naver,com>
2025-04-04 10:17:57 +0900

    :hammer: Fix: 네이베이션 수정

--- frontend/react-native/app/src/navigation/AppNavigator.tsx
+++ frontend/react-native/app/src/navigation/AppNavigator.tsx
@@ -45,6 +45,8 @@ const AppNavigator = () => {
         <Stack.Screen name="Service" component={ServiceScreen} />
         <Stack.Screen name="Main" component={TabNavigator} />
         <Stack.Screen name="PlayerSelect" component={PlayerSelectScreen} />
+        <Stack.Screen name="AccountSelect" component={AccountSelectScreen} />
+        <Stack.Screen name="Completion" component={CompletionScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
